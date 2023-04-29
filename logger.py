151480ad234bb761e7e29873eaf17685d401bfbc
pixcelo <bodomu@gmail.com>
2023-04-30 03:39:23 +0900

    logger

--- logger.py
+++ logger.py
@@ -1,4 +1,19 @@
+import os
+from loguru import logger
+
 class Logger:
-    def log(self, trade_result, prediction):
-        # Log trade_result and prediction here
-        pass
+    def __init__(self, log_name, log_level="INFO"):
+        logger.remove()
+        logger.add(
+            f"log/{log_name}.log",
+            level="INFO",
+            format="{time} - {name} : [{level}] {message}",
+            rotation="10 MB"
+        )
+
+        # Create log folder if it doesn't exist
+        if not os.path.exists("log"):
+            os.makedirs("log")
+
+    def __call__(self):
+        return logger
