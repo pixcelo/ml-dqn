7395eba5e8b3bf9a74074a48197b7fb30093435a
pixcelo <bodomu@gmail.com>
2023-04-30 11:54:45 +0900

    discord

--- discord_notifier.py
+++ discord_notifier.py
@@ -1,9 +1,9 @@
-import requests
+from discord_webhook import DiscordWebhook
 
 class DiscordNotifier:
     def __init__(self, config):
         self.webhook_url = config.get("discord", "webhook_url")
 
-    def notify(self, trade_result, prediction):
-        # Send trade_result and prediction to Discord channel here
-        pass
+    def notify(self, message):
+        webhook = DiscordWebhook(url=self.webhook_url, content=message)
+        webhook.execute()
