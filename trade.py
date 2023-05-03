852cf4edbbab2adf14c8b278ae5693f52ec5b114
pixcelo <bodomu@gmail.com>
2023-05-04 04:34:24 +0900

    set position mode

--- trade.py
+++ trade.py
@@ -23,10 +23,11 @@ class Trade:
             "enableRateLimit": True,
         })
 
-        self.logger = Logger("main")
+        self.logger = Logger("trade")
         self.recv_window=str(10000)
         self.url="https://api.bybit.com"
         # self.url="https://api-testnet.bybit.com" 
+        self.mode = self.set_position_mode(0)
 
     def get_ohlcv(self, timeframe):
         ohlcv = self.exchange.fetch_ohlcv("BTC/USDT", timeframe)
@@ -52,7 +53,6 @@ class Trade:
         result = None
 
         trade_action = self.decide_trade_action(long_positions, short_positions, prediction)
-        print(trade_action)
 
         if trade_action == "BUY_TO_OPEN":
             result = self.place_order("BTCUSDT", "Buy", amount)
@@ -65,7 +65,17 @@ class Trade:
         elif trade_action == "HOLD_LONG" or trade_action == "HOLD_SHORT" or trade_action == "DO_NOTHING":
             pass
 
-        return result
+        message = f"{trade_action}: "
+
+        if result is True:
+            message += "The order was successful."
+        elif result is False:
+            message += "The order was failed."
+        else:
+            message += "Hold current position."
+        
+        print(message)
+        return message
     
     def decide_trade_action(self, long_positions, short_positions, prediction):
         if long_positions:
@@ -99,15 +109,13 @@ class Trade:
         params = {
             "symbol": symbol,
             "side": side,
-            "positionIdx": 1,
             "orderType": "Market",
             "qty": str(amount),
             "timeInForce": "GoodTillCancel",
-            "orderLinkId": orderLinkId
+            "orderLinkId": orderLinkId,
+            "positionIdx": 0 # one-way mode
         }
-        self.http_request(endpoint, method, params, "Create")
-        return True
-    
+        return self.http_request(endpoint, method, params, "Create")    
     
     def http_request(self, endpoint, method, params, info):
         try:
@@ -130,19 +138,27 @@ class Trade:
                 payload = urlencode(params)
                 response = httpClient.request(method, self.url + endpoint + '?' + payload, headers=headers)
 
-            response_data = response.json()
-
             if response.status_code == 200:
-                summary = f"{info} succeeded. Elapsed Time: {response.elapsed}, Result: {response_data}"
+                if response.text:
+                    response_data = response.json()
+                    summary = f"{info} succeeded. Elapsed Time: {response.elapsed}, Result: {response_data}"
+                else:
+                    summary = f"{info} succeeded. Elapsed Time: {response.elapsed}, Result: No response data"
             else:
-                summary = f"{info} failed. Elapsed Time: {response.elapsed}, Error: {response_data}"
+                if response.text:
+                    response_data = response.json()
+                    summary = f"{info} failed. Elapsed Time: {response.elapsed}, Error: {response_data}"
+                else:
+                    summary = f"{info} failed. Elapsed Time: {response.elapsed}, Result: No response data"
 
+            self.logger().info(summary)
             print(summary)
-            return summary
+            return True
 
         except Exception as e:
             self.logger().error(f"An exception occurred: {e}")
-            return f"{info} failed. An exception occurred: {e}"
+            print(f"{info} failed. An exception occurred: {e}")
+            return False
 
 
     def get_best_bid_ask_price(self, symbol, side):
@@ -152,38 +168,22 @@ class Trade:
         else:
             return order_book['ask'][0][0]
         
+    def set_position_mode(self, mode):
+        endpoint = "/v5/position/switch-mode"
+        method = "POST"
+        params = {
+            "category": "linear",
+            "symbol": "BTCUSDT",
+            "coin": "USDT",
+            "mode": mode  # Position mode. 0: Merged Single. 3: Both Sides
+        }
+        response = self.http_request(endpoint, method, params, "Set Position Mode")
+        return response
+        
     def get_balance(self):
         balance = self.exchange.fetch_balance()
         usdt_balance = balance['total']['USDT']
         print(f'USDT balance: {usdt_balance}')
-
-    # def http_request(self, endpoint, method, params, info):
-    #     try:
-    #         httpClient = requests.Session()
-    #         global time_stamp
-    #         time_stamp = str(int(time.time() * 10 ** 3))
-    #         payload = json.dumps(params)
-    #         signature = self.genSignature(payload)
-    #         headers = {
-    #             'X-BAPI-API-KEY': self.api_key,
-    #             'X-BAPI-SIGN': signature,
-    #             'X-BAPI-SIGN-TYPE': '2',
-    #             'X-BAPI-TIMESTAMP': time_stamp,
-    #             'X-BAPI-RECV-WINDOW': self.recv_window,
-    #             'Content-Type': 'application/json'
-    #         }
-    #         if method == "POST":
-    #             response = httpClient.request(method, self.url + endpoint, headers=headers, data=payload)
-    #         else:
-    #             payload = urlencode(params)
-    #             response = httpClient.request(method, self.url + endpoint + '?' + payload, headers=headers)
-    #         print(response.text)
-    #         print(info + " Elapsed Time : " + str(response.elapsed))
-    #         return True
-        
-    #     except Exception as e:
-    #         self.logger().error(f"An exception occurred: {e}")
-    #         return False
 
     def genSignature(self, payload):
         param_str= str(time_stamp) + self.api_key + self.recv_window + payload
