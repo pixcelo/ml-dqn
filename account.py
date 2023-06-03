16b14b31a3ba855d7a8f0014ec7763e74a89b582
pixcelo <bodomu@gmail.com>
2023-06-03 13:55:59 +0900

    mod print format

--- account.py
+++ account.py
@@ -5,26 +5,26 @@ class Account:
     def get_balance(self):
         balance = self.exchange.fetch_balance()
         
-        print('='*50)
+        print('='*40)
         usdt_balance = balance['total']['USDT']
-        print(f'USDT balance: {usdt_balance}')
+        print(f'USDT balance: {round(usdt_balance, 2)}')
         
         btc_balance = balance['total']['BTC']
-        print(f'BTC balance: {btc_balance}')
+        # print(f'BTC balance: {round(btc_balance, 2)}')
         
         margin_balance = self.exchange.fetch_balance(params={'type': 'margin'})
         usdt_margin_balance = margin_balance['total']['USDT']
-        print(f'USDT margin balance: {usdt_margin_balance}')
+        print(f'USDT margin balance: {round(usdt_margin_balance, 2)}')
         btc_margin_balance = margin_balance['total']['BTC']
-        print(f'BTC margin balance: {btc_margin_balance}')
+        # print(f'BTC margin balance: {round(btc_margin_balance, 2)}')
         
         free_margin = margin_balance['free']['USDT']
-        print(f'Free margin (USDT): {free_margin}')
+        print(f'Free margin (USDT): {round(free_margin, 2)}')
         
         if usdt_margin_balance != 0:
             margin_utilization_rate = (usdt_balance - free_margin) / usdt_margin_balance * 100
-            print(f'Margin utilization rate: {margin_utilization_rate}%')
+            print(f'Margin utilization rate: {round(margin_utilization_rate, 2)}%')
         else:
             print("No margin balance available.")
 
-        print('='*50)
+        print('='*40)
